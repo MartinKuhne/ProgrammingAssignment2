096499a22124b60301f363b51e70a96df6a1b5e4
MartinKuhne <mkuhne@outlook.com>
2014-10-07 22:10:29 -0700

    Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## reviewer note: I've called makeCacheMatrix makeProxy for a more correct
+## and descriptive name
 
-## Write a short comment describing this function
+# makeProxy - returns a list of functions to get and set an object
+# and get and set an associated object
+# Parameters - object: object you want to decorate
+# Returns - list of functions: getObject, setObject, getMeta, setMeta
+# Notes: The associated object is called Meta here, alluding to "Metadata"
+# This is a generalized and cleaned up version of the makeVector/makeCacheMatrix () provided
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeProxy <- function(object)
+{
+    cachedMeta <- NULL
+    
+    # return the encapsulated object
+    getObject <- function()
+    {
+        object
+    }
+    
+    # setting the encapsulated object clears the Meta
+    setObject <- function(newObject)
+    {
+        object <<- newObject
+        cachedMeta <<- NULL
+    }
+    
+    # retrieve the Meta
+    getMeta <- function()
+    {
+        cachedMeta
+    }
+    
+    # update the Meta
+    setMeta <- function(newMeta)
+    {
+        cachedMeta <<- newMeta
+    }
+    
+    list(getObject = getObject,
+         setObject = setObject,
+         getMeta = getMeta,
+         setMeta = setMeta)
 }
 
+# cacheSolve - returns the inverse of a matrix, caching the result for repeat calls
+# Parameters - proxy: proxy obtained from makeProxy()
+# Returns - inverted matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(proxy)
+{
+    cachedValue <- proxy$getMeta()
+    if(!is.null(cachedValue)) {
+        message("getting cached data")
+        return(cachedValue)
+    }
+    
+    data <- proxy$getObject()
+    computedValue <- solve(data)
+    proxy$setMeta(computedValue)
+    computedValue
 }
+
